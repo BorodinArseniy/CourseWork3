d26d09bcf9725366eebc8e41b7918c3c6c678a43
BorodinArseniy <b9692861615@gmail.com>
2023-03-12 15:07:38 +0300

    1

--- src/main/java/com/example/coursework3/controllers/SocksController.java
+++ src/main/java/com/example/coursework3/controllers/SocksController.java
@@ -11,7 +11,7 @@ import javax.validation.Valid;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/socks")
+@RequestMapping("/api/socks")
 public class SocksController {
     private final SockService sockService;
 
@@ -19,43 +19,31 @@ public class SocksController {
         this.sockService = sockService;
     }
 
-    @GetMapping("/{num}")
-    public ResponseEntity<Sock> getSocks(@PathVariable Integer num) {
-        if (sockService.getSocks(num) == null) {
+    @GetMapping("/{sock}")
+    public ResponseEntity<Sock> getSocks(@PathVariable Sock sock) {
+        if (sockService.getNumberOfSocks(sock) == null) {
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok(sockService.getSocks(num));
+        return ResponseEntity.ok(sock);
     }
-
-    @GetMapping("/allSocks")
-    public ResponseEntity<Map<Integer, Sock>> getIAllSocks() {
-        if (sockService.getAllSocks() == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(sockService.getAllSocks());
-    }
-
     @PostMapping("/putNewSocks")
     public ResponseEntity<Sock> addNewSocks(@Valid @RequestBody Sock sock){
         sockService.addSocks(sock);
         return ResponseEntity.ok(sock);
     }
 
-    @PutMapping("/{num}")
-    public ResponseEntity<Sock> editSocks(@PathVariable Integer num, @Valid @RequestBody Sock sock){
-        if (sockService.getSocks(num) == null) {
-            return ResponseEntity.notFound().build();
-        }
-        sockService.changeSock(num, sock);
-        return ResponseEntity.ok(sockService.getSocks(num));
+    @PostMapping("/sellSocks")
+    public ResponseEntity<Sock> sellSocks(@Valid @RequestBody Sock sock){
+        sockService.sellSocks(sock);
+        return ResponseEntity.ok(sock);
     }
 
-    @DeleteMapping("/{num}")
-    public ResponseEntity deleteIngredient(@PathVariable Integer num) {
-        if (sockService.getSocks(num) == null) {
+    @DeleteMapping("/{sock}")
+    public ResponseEntity deleteSocks(@PathVariable Sock sock) {
+        if (sockService.getNumberOfSocks(sock) == null) {
             return ResponseEntity.notFound().build();
         }
-        sockService.deleteSock(num);
+        sockService.deleteSock(sock);
         return ResponseEntity.ok().build();
     }
 }
